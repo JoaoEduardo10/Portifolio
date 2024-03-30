5d0f6930278d3aba2b1cdbce647516daed483e5f
João Eduardo <joaoeduardoj920@gmail.com>
2024-03-30 14:27:51 -0300

    feat(<Heading />): adding new components

--- src/components/heading/heading.test.tsx
+++ src/components/heading/heading.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Heading } from '.';
 import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 describe('<Heading />', () => {
   it('should render Heading componet with text', () => {
@@ -25,5 +27,43 @@ describe('<Heading />', () => {
     expect(text).not.toBeInTheDocument();
     expect(image).toBeInTheDocument();
     expect(navigation).toBeInTheDocument();
+  });
+
+  it('should show a button for the mobile', () => {
+    window.matchMedia = vi.fn().mockImplementation((query) => ({
+      matches: query !== '(max-width: 749px)',
+      media: '',
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    }));
+
+    render(<Heading text="Desenvolvedor" logo={'http://logo.png'} />);
+
+    const buttonMobile = screen.getByLabelText('button mobile');
+
+    expect(buttonMobile).toHaveStyle({
+      display: 'block',
+    });
+  });
+
+  it('should open menu', async () => {
+    window.matchMedia = vi.fn().mockImplementation((query) => ({
+      matches: query !== '(max-width: 749px)',
+      media: '',
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    }));
+
+    render(<Heading text="Desenvolvedor" logo={'http://logo.png'} />);
+
+    const buttonMobile = screen.getByLabelText('button mobile');
+
+    expect(buttonMobile).toHaveStyle({
+      display: 'block',
+    });
+
+    await userEvent.click(buttonMobile);
   });
 });
--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -1,10 +1,12 @@
-'use server';
+'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import { Navigation } from '../navigation';
 import { navItensMock } from './navItensMock';
+import { ButtonMenu } from '../buttons/menu';
+import { MenuMobile } from '../menuMobile';
 
 export type HeadingProps = {
   logo?: string | StaticImport;
@@ -12,18 +14,28 @@ export type HeadingProps = {
 };
 
 export const Heading = ({ logo, text }: HeadingProps) => {
+  const [show, setShow] = useState(false);
+
+  const handleShowMenu = (show: boolean) => {
+    setShow(show);
+  };
+
   return (
-    <header>
-      {logo ? (
-        <div aria-label="logo" className="logo">
-          <Image src={logo} alt="Logo" width={100} height={100} />
-        </div>
-      ) : (
-        <p aria-label="Heading test" className="logoTitle">
-          {text}
-        </p>
-      )}
-      <Navigation navItens={navItensMock} />
-    </header>
+    <div className="conteiner-header">
+      <header>
+        {logo ? (
+          <div aria-label="logo" className="logo">
+            <Image src={logo} alt="Logo" width={100} height={100} />
+          </div>
+        ) : (
+          <p aria-label="Heading test" className="logoTitle">
+            {text}
+          </p>
+        )}
+        <Navigation navItens={navItensMock} />
+        <ButtonMenu show={show} handleShow={handleShowMenu} />
+      </header>
+      {show && <MenuMobile handleShowMenu={handleShowMenu} />}
+    </div>
   );
 };
