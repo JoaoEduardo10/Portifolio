a7c411b69aefc1652fc3cd18300e1562b58f3190
João Eduardo <joaoeduardoj920@gmail.com>
2024-05-08 15:12:08 -0300

    feat: adicinando efeito de digitação

--- src/template/home.tsx
+++ src/template/home.tsx
@@ -1,7 +1,16 @@
+'use client';
+
 import { Button } from '@/components/buttons/template-home';
 import React from 'react';
+import { Cursor, useTypewriter} from 'react-simple-typewriter'
 
 export const TemplateHome = () => {
+  const [text] = useTypewriter({
+    words: ['Uma mistura de Desenvolvimento Frontend, Back-End e habilidade profissional que vão destacar seu produto no mercado.'],
+    loop: 1,
+    typeSpeed: 10
+  });
+
   return (
     <section className="conteiner-template-home">
       <h1>
@@ -9,8 +18,7 @@ export const TemplateHome = () => {
         de forma moderna e de alta qualidade.{' '}
       </h1>
       <p>
-        Uma mistura de Desenvolvimento Frontend, Back-End e habilidade
-        profissional que vão destacar seu produto no mercado.{' '}
+        {text}<Cursor cursorColor='#986dff' />
       </p>
       <Button text="Entre em contato" />
     </section>
