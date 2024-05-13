d539ba8a55d93d106a928a34292e302d86cec654
João Eduardo <joaoeduardoj920@gmail.com>
2024-05-13 09:33:12 -0300

    feat: adding experience session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Heading } from '@/components/heading';
 import { TemplateHome } from '@/template/home';
 import { Sobre } from '@/components/sobre';
+import { Conteiner } from '@/components/conteiner';
 
 export default function Home() {
   return (
@@ -12,6 +13,36 @@ export default function Home() {
           <TemplateHome />
         </div>
         <Sobre />
+        <Conteiner>
+          <article className="conteiner-experiencia">
+            <h2>
+              Experiencias <span>.</span>
+            </h2>
+            <div className="experiencia">
+              <ul className="experiencia-menu">
+                <li>Interativa</li>
+                <li>pipoca Agilc</li>
+                <li>pipoca Agilc</li>
+                <li>pipoca Agilc</li>
+              </ul>
+              <div className="experiencia-conteiner-text">
+                <h3>
+                  Programador de Sistemas
+                  <span>Nov 2023</span>
+                </h3>
+                <h4>interativa</h4>
+
+                <p>
+                  Trabalhei como instrutor conteudista em frontend na Digital
+                  House, o trabalho consistia em criar conteúdos para as aulas
+                  de especialização frontend. Alguns temas dos conteúdos
+                  realizados foram: React, Redux, TypeScript, Testes, GraphQL,
+                  Next.js, MUI, React Hook Form e styled-components.
+                </p>
+              </div>
+            </div>
+          </article>
+        </Conteiner>
       </main>
     </div>
   );
--- src/components/menuMobile/menuMobile.stories.tsx
+++ src/components/menuMobile/menuMobile.stories.tsx
@@ -5,10 +5,13 @@ import { MenuMobile } from '.';
 const meta = {
   title: 'Component/menuMobile',
   component: MenuMobile,
+  args: {
+    handleShowMenu: () => alert('ok'),
+  },
 } satisfies Meta<typeof MenuMobile>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const ComponentMenuMobile: Story = {};
+export const MenuMobileStory: Story = {};
